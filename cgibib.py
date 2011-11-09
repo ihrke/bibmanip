f25913c006270a814620a04cc4cba253b3f43d77
Matthias Ihrke <ihrke@nld.ds.mpg.de>
2011-11-09 13:42:07 +0100

    began coding

--- cgibib.py
+++ cgibib.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 import cgi, sys, time
+from bibparse import *
+
 DEBUG=True
 if DEBUG: import cgitb; cgitb.enable()  # debug
-
 form=cgi.FieldStorage()
 
 print "Content-type: text/html\n"
 
-print "HI!"
+BIBFILE=form["bibtex-file"].value if form.has_key("bibtex-file") else "master.bib"
+
+print "Using file: %s"%BIBFILE
+bib=BibtexFile( BIBFILE )
+bib.parse()
+print "<ul>"
+for e in bib.bibentries:
+    print "<li>%s</li>"%e.key
+print "</ul>"
